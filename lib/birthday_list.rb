028350212fbacf5a4d44b2f153e0a970b1689a7c
Otis <otisvickersgraver@gmail.com>
2020-09-29 12:33:44 +0100

    add_birthday method passes tests

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,4 +1,10 @@
 class BirthdayList
+
+  def initialize
+    @list = []
+  end
+
   def add_birthday(name, birthday)
+    @list << {name: name, birthday: birthday}
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,9 +2,9 @@ require 'birthday_list'
 
 # Store all of my friends’ birthdays so I can keep track of them
 # Add name to a list, this name should have a value of the persons birthday
-# add_name(name, birthday)
+# add_birthday(name, birthday)
 
-describe BirthdayList do
+RSpec.describe BirthdayList do
   it "adds a person's birthday to list" do
     test_birthdays = BirthdayList.new
     expect(test_birthdays.add_birthday("Otis", "25 May")).to eq [{name: "Otis", birthday: "25 May"}]
