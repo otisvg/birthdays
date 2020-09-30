58b38acb57df648bdd84beee91bae5308db5baab
Otis <otisvickersgraver@gmail.com>
2020-09-30 12:49:24 +0100

    show_birthdays prints birthday list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -8,4 +8,10 @@ class BirthdayList
     @list << {name: name, birthday: birthday}
   end
 
+  def show_birthdays
+    @list.each do |person|
+      puts "#{person[:name]}: #{person[:birthday]}"
+    end
+  end
+
 end
